d7562c57f09ac63d459456c6d7861ec79a0bef3c
WI-Hyun-joong <wjh4877@naver.com>
2018-01-12 15:14:14 +0900

    insert, share 수정 / item.getCenter 수정

--- quedTree.js
+++ quedTree.js
@@ -8,7 +8,8 @@ function item(maxX, maxY, minX, minY) {
 }
 
 function QuedTree(item) {
-    this.item = null;
+    this.item = item;
+    this.leaf = true;
     this.quedArray = new Array();
 }
 
@@ -16,7 +17,10 @@ QuedTree.prototype = {
 
     insert: function (item) {
         if (this.quedArray.length == ARRAY_SIZE) {
-            let itemCenter = item.getCenter(item);
+            if (this.leaf) {
+                this.share();
+            }
+            let itemCenter = item.getCenter();
             for (let node of this.quedArray) {
                 if ((itemCenter.centerX < node.maxX && itemCenter.centerX > node.minX)
                     && (itemCenter.centerY < node.maxY && itemCenter.centerY > node.minY))
@@ -36,8 +40,8 @@ QuedTree.prototype = {
                 && item.maxY == node.maxY && item.minY == node.minY)
                 return console.timeEnd('search time : ');
 
-            else if ((center.centerX < node.maxX && center.centerX > node.minX)
-                && (center.centerY < node.maxY && center.centerY > node.minY))
+            else if ((center.centerX < node.item.maxX && center.centerX > node.item.minX)
+                && (center.centerY < node.item.maxY && center.centerY > node.item.minY) && !(this.leaf))
                 return node.search(item, center);
 
             else
@@ -46,39 +50,39 @@ QuedTree.prototype = {
         }
     },
     share: function () {
-        let nodeCenter = item.prototype.getCenter(this.item);
+        this.leaf = false;
+
+        let nodeCenter = this.item.getCenter();
         let tempDataArray = this.quedArray.slice();
 
-        let itemCoodinater = [item(nodeCenter.centerX, nodeCenter.centerY, this.item.minX, this.item.minY), //2사분면
-                              item(this.item.maxX, nodeCenter.centerY, nodeCenter.centerX, this.item.minY), //1사분면
-                              item(nodeCenter.centerX, this.item.maxY, this.item.minX, nodeCenter.centerY), //3사분면
-                              item(this.item.maxX, this.item.maxY, nodeCenter.centerX, nodeCenter.centerY)] //4사분면
+        let itemCoodinater = [new item(nodeCenter.centerX, nodeCenter.centerY, this.item.minX, this.item.minY), //2사분면
+                              new item(this.item.maxX, nodeCenter.centerY, nodeCenter.centerX, this.item.minY), //1사분면
+                              new item(nodeCenter.centerX, this.item.maxY, this.item.minX, nodeCenter.centerY), //3사분면
+                              new item(this.item.maxX, this.item.maxY, nodeCenter.centerX, nodeCenter.centerY)] //4사분면
         ''
-        for(let i = 0; i < ARRAY_SIZE; i++){
+        for (let i = 0; i < ARRAY_SIZE; i++) {
             this.quedArray[i] = new QuedTree(itemCoodinater[i]);
             let offset = 0;
-            for(let item of tempDataArray){
-                if ((item.centerX < this.quedArray[i].maxX && item.centerX > this.quedArray[i].minX)
-                && (item.centerY < node.maxY && this.quedArray[i].centerY > this.quedArray[i].minY)){
-                    this.quedArray[i].push(item);
-                    tempDataArray.splice(offset,1);
+            for (let item of tempDataArray) {
+                if ((item.centerX < this.quedArray[i].item.maxX && item.centerX > this.quedArray[i].item.minX)
+                    && (item.centerY < this.quedArray[i].item.maxY && item.centerY > this.quedArray[i].item.minY)) {
+                    this.quedArray[i].quedArray.push(item);
+                    tempDataArray.splice(offset, 1);
                     break;
                 }
                 offset++;
             }
         }
-
-
-    }
+    },
 
 }
 
 item.prototype = {
-    
-    getCenter: function (item) {
+
+    getCenter: function () {
         return {
-            centerX: (this.maxX + this.minX) / 2,
-            centerY: (this.maxY + this.minY) / 2
+            centerX: Number(((this.maxX + this.minX) / 2).toFixed(9)),
+            centerY: Number(((this.maxY + this.minY) / 2).toFixed(9))
         }
     }
 }
